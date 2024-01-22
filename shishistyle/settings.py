a12e0abbf9aa9ec579d8fcc37668f22b9b0b5a05
Anurag Singhal <anurag7noni@gmail.com>
2024-01-22 01:34:55 -0800

    Fix CSS

--- shishistyle/settings.py
+++ shishistyle/settings.py
@@ -140,7 +140,7 @@ STATIC_URL = "static/"
 # Add these new lines
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles", "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
