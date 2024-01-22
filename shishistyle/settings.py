68dafbd825ac53af02beae22d63d92945d75f382
Anurag Singhal <anurag7noni@gmail.com>
2024-01-22 00:26:30 -0800

    Fix wsgi

--- shishistyle/settings.py
+++ shishistyle/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = quote_plus(os.environ['secret_key']) 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ['debug']
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
