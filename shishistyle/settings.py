a24af84c6a410dd31c7de20b2180a75f32e68ec2
Anurag Singhal <anurag7noni@gmail.com>
2024-01-22 01:23:51 -0800

    Fix CSS

--- shishistyle/settings.py
+++ shishistyle/settings.py
@@ -137,11 +137,8 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-# # Add these new lines
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static",
-#     "/var/www/static/",
-# ]
+# Add these new lines
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
