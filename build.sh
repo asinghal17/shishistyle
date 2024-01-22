38478da1a6db63cc89620b7dd17526884710bfd9
Anurag Singhal <anurag7noni@gmail.com>
2024-01-22 00:56:36 -0800

    Fix wsgi

--- build.sh
+++ build.sh
@@ -14,6 +14,6 @@ python3 -m pip install -r requirements.txt
 # echo "Migrating..."
 # python3 manage.py migrate
 
-# # Collect static files
-# echo "Collecting static files..."
-# python3 manage.py collectstatic --noinput
+# Collect static files
+echo "Collecting static files..."
+python3 manage.py collectstatic --noinput
